4bbdde3f6191309c50781692b739f53eaf92066d
Raphaël Claude <charlesalbert.claude@gmail.com>
2012-10-30 00:05:44 +0800

    collaborative drawing

---
+++ server.js
@@ -0,0 +1,64 @@
+var ws = require('ws');
+
+var server = new ws.Server({ port: +process.argv[2] || 1234 });
+
+var sockets = [];
+var objects = [];
+
+function send(socket, event, data) {
+  if (socket) {
+    socket.send(JSON.stringify({ event: event, data: data }));
+  }
+}
+
+server.on('connection', function(socket) {
+
+  console.log('connected');
+
+  var client = sockets.length,
+      handle = undefined;
+
+  sockets.push(socket);
+
+  socket.on('open', function () {
+    send(socket, 'state', objects); // a key frame
+  });
+
+  socket.on('message', function (data) {
+    var message = JSON.parse(data);
+    socket.emit(message.event, message.data);
+  });
+  
+  socket.on('point', function (data) {
+    console.log('point', client, data, 'object', handle, 'in', objects.length);
+    if (handle === undefined) {
+      handle = objects.length;
+      objects.push([]);
+      send(socket, 'handle', handle);
+    }
+    
+    objects[handle].push(data);
+
+    for (var i in sockets) {
+      if (i != client) {
+        send(sockets[i], 'point', {
+          handle: handle,
+          point: data
+        });
+      }
+    }
+  });
+
+  socket.on('finalize', function (data) {
+    console.log('finalize', client, data);
+    handle = undefined;
+  });
+
+  socket.on('close', function () {
+    console.log('close', client);
+    sockets = sockets.splice(sockets.indexOf(socket));
+  });
+});
+
+
+
